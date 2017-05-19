65dcdefd1e711c19ba8b80ebe552c6489afbeb3b
Rima Alrwais <ralrwais@ralrwais-ltm2.internal.salesforce.com>
2017-05-18 18:08:01 -0700

    Consolidated on theme and added form to enter name

--- views/js/my-fitness.js
+++ views/js/my-fitness.js
@@ -1,6 +1,71 @@
+//special effect to load page
+
 function main() {
 	var main = $(".main").hide();
 	$(".main").fadeIn(2000);
 	
 };
 $(document).ready(main);
+
+//code that takes in values from forms
+
+var calEaten = document.getElementById("calInput").value;
+var calBurn = $("#formBurned").val().trim();
+var total = document.getElementById('total');
+
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+var modal2 = document.getElementById('myModal2');
+var btn2 = document.getElementById("myBtn2");
+var span2 = document.getElementsByClassName("close2")[0];
+
+
+
+$("#myBtn").on("click", function(){
+	console.log("Button Click");
+	var caleaten = $("input.calEaten").val().trim();
+	if (caleaten > 0) {
+	console.log(caleaten);
+	caleaten.innerHtml(total);
+  }
+})
+
+
+//button for adding calories
+
+
+btn.onclick = function() {
+	 
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+ 
+//button for subtracting calories
+
+btn2.onclick = function() {
+    modal2.style.display = "block";
+}
+
+span2.onclick = function() {
+    modal2.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal2) {
+        modal.style.display = "none";
+    }
+}
+
+
+
