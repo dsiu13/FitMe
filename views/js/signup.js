695cd182e3044681a6d6a1115aa208a8e3fee587
Rima Alrwais <ralrwais@ralrwais-ltm2.internal.salesforce.com>
2017-05-19 20:26:03 -0700

    Lookin fresh

---
+++ views/js/signup.js
@@ -0,0 +1,27 @@
+$(document).on("submit", "#signup", signUpForm);
+        function updateUsers(){
+            $.get("/api/dashboard/user/postUser", function(){
+                console.log("signup", data);
+                signup = data;
+            })
+        }
+        function signUpForm(){
+            console.log("on click sub works")
+       
+       event.preventDefault();
+         var signup = {
+         UserName: $("input.UserName").val().trim(),
+         Password: $("input.pw").val().trim(),
+         height: $("input.height").val().trim(),
+         cWeight: $("input.cWeight").val().trim(),
+         gWeight: $("input.gWeight").val().trim()
+         }
+
+            console.log(signup);
+            $.post("/signup", signup, function(req, res) {
+            console.log(arguments)
+            console.log("Click Works");
+       		}) 
+ 
+        
+    };
