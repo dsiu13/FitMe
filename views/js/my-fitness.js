46ba1bb164caa1b8bd3d90b714e41bf937e4a289
Chris French <clfrench84@gmail.com>
2017-05-20 09:22:51 -0700

    some stuff

--- views/js/my-fitness.js
+++ views/js/my-fitness.js
@@ -24,26 +24,35 @@ var span2 = document.getElementsByClassName("close2")[0];
 var calEaten = document.getElementById("calInput").value;
 var foodEaten = document.getElementById("calInput1").value;
 
-var calBurned = document.getElementById("formBurned").value;
+var calBurned1 = document.getElementById("formBurned").value;
 var exercises = document.getElementById("formBurned1").value;
 
-var calBurn = $("#formBurned").val().trim();
+// var calBurn = $("#formBurned").val().trim();
 
 
  var currentUser = "chris";
 
- var calories = [];
+ // var gainedCalories = [];
+ var gainedCalories = [].reduce(add, 0);
+ function add(a, b) {
+   return a + b;
+ }
+ 
+
+ var lostCalories = [];
 
 //on click adds numbers to total
 
 $("#myBtn").on('click', function(){
-
+    var cal = gainedCalories;
     var start = [];
     var getNumber = $("input.calEaten").val().trim();
     var total = parseInt(getNumber);
     start = start + total;
-    $(".totalGain").html(start);
+    cal.push(start);
     console.log(start);
+    $(".totalGain").html(gainedCalories);
+
 });
 
 
@@ -54,6 +63,7 @@ $("#myBtn").on("click", function(){
     var foodData = data[0].FoodCalorieGain;
     console.log(foodData);
     $(".totalGain").html(foodData);
+
     // foodData = calories;
     // }).then(function() {
     // $.post("/api/dashboard/user/caloriesIn/"+calories, function(data) {
@@ -69,7 +79,7 @@ $("#myBtn").on("click", function(){
 $("#myBtn2").on('click', function(){
 
     var start = [];
-    var getNumber = $("input.calBurned").val().trim();
+    var getNumber = $("input.calBurned1").val().trim();
     console.log(getNumber);
     var total = parseInt(getNumber);
     start = start + total;
@@ -92,7 +102,11 @@ function getFood(foodname, res) {
     $.get("/api/dashboard/food/findOne/", function(data) {
       console.log(data);
 });
-};    
+}; 
+
+function sumTotal() {
+    gainedCalories - lostCalories;
+}   
 
 
 //button for adding calories
